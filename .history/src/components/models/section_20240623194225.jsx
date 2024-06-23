b4633a6c553f23b70fa522e29a37d4cbbb4867b1
Tracy-maker <ydlvns@gmail.com>
2024-06-23 19:43:14 +1000

    fix layout

--- .history/src/components/models/section_20240623194225.jsx
+++ .history/src/components/models/section_20240623194225.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Section = (props) => {
   const { children } = props;
   return (
-    <div className="h-screen w-screen pt-10 p-4 max-w-screen-2xl mx-auto flex flex-col lg:flex-row items-start justify-between">
+    <div className="h-screen w-screen pt-10  max-w-screen-2xl mx-auto flex flex-col lg:flex-row items-start justify-between">
       {children}
     </div>
   );
