006b50c89dc5d43a349b02248cbd86bcbb70c9b1
Tracy-maker <ydlvns@gmail.com>
2024-06-23 19:29:19 +1000

    fix errors

---
+++ .history/src/components/models/Card_20240623192905.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = (props) => {
+  const { image, title } = props;
+
+  return (
+    <div className="overflow-hidden transform transition duration-500 hover:scale-105 hover:shadow-xl">
+      <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-128 sm:h-144 md:h-160 lg:h-176 xl:h-192 mx-auto">
+        <div className="w-full h-full overflow-hidden">
+          <img src={image} alt={title} className="w-full h-full object-cover" />
+        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
+          <h3 className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-white text-center px-4 py-2 bg-opacity-75 rounded">
+            {title}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
